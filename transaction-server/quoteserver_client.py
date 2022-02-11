545d581bf1900c70333b22a75c1ff2770bd1bf92
Anton Nikitenko <anikitenko@uvic.ca>
2022-02-11 00:20:12 -0800

    Implemented working quote server static method

--- transaction-server/quoteserver_client.py
+++ transaction-server/quoteserver_client.py
@@ -7,18 +7,29 @@ PORT = 4444
 class QuoteServerClient():
 
     @staticmethod
-    def get_quote(symbol):
+    def get_quote(symbol, username):
         '''
         Get price of stock by specified symbol.
 
         Parameter:
             symbol (str): The stock's symbol
+            username (str): Username associated with originating request
         Returns
             price (float): Stock price
+            symbol (str): Symbol returned.
+            username (str): Returned username (?)
+            timestamp (int): Time request was processed (UNIX timestamp)
+            cryptographickey (str): (?)
         '''
+        assert str(symbol) == str
+        assert str(username) == str
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
             s.sendall(str.encode('SYM {}'.format(symbol)))
             data = s.recv(1024)
 
+        data_str_trimmed = data.decode('utf-8')[:-1]
+        price, symbol, username, timestamp, cryptographickey = data_str_trimmed.split(',')
+
+        return float(price), symbol, username, int(timestamp), cryptographickey
